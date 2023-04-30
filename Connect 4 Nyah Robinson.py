0b586a14a05250de6244fd4f26378d8fd6e35c31
nrobin56 <122395898+nrobin56@users.noreply.github.com>
2023-04-30 17:37:31 -0400

    Update Connect 4 Nyah Robinson.py

--- Connect 4 Nyah Robinson.py
+++ Connect 4 Nyah Robinson.py
@@ -134,7 +134,7 @@ while not game_over:
                         screen.blit(label, (40,10))
                         game_over = True
 
-            print_board(board)
+            `print_board(board)
             draw_board(board)
 
             turn += 1
